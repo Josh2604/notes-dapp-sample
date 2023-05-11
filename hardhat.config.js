92dcbc29eb5c67d40cd870f7f39d63e7ec2b715b
Josh2604 <josh.an.bas@gmail.com>
2023-05-10 23:50:22 -0600

    feature: avalanche fuji tesnet added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config()
 
-const MUMBAI_URL= process.env.MUMBAI_URL;
+const MUMBAI_URL = process.env.MUMBAI_URL;
 const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
 
+const FUJI_URL = process.env.FUJI_URL;
+const FUJI_PRIVATE_KEY = process.env.FUJI_PRIVATE_KEY;
 // console.log("------")
 // console.log("Mumbai url: ", MUMBAI_URL)
+// console.log("Fuji url: ", FUJI_URL)
 // console.log("------")
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -16,6 +19,10 @@ module.exports = {
     mumbai: {
       url: MUMBAI_URL,
       accounts: [MUMBAI_PRIVATE_KEY]
+    },
+    fuji: {
+      url: FUJI_URL,
+      accounts: [FUJI_PRIVATE_KEY],
     }
   }
 };
--- scripts/TaskList.js
+++ scripts/TaskList.js
@@ -3,16 +3,19 @@ const abi = require("../artifacts/contracts/TaskList.sol/TaskList.json");
 
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const MUMBAI_URL = process.env.MUMBAI_URL;
-const PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
+
+const FUJI_URL = process.env.FUJI_URL;
+const FUJI_PRIVATE_KEY = process.env.FUJI_PRIVATE_KEY;
 
 async function main(){
   const contractAddress = CONTRACT_ADDRESS;
   const contractABI = abi.abi;
 
   // const provider = new hre.ethers.providers.EtherscanProvider("maticmum", process.env.MATIC_PRIVATE_KEY);
-  const provider = new hre.ethers.providers.JsonRpcProvider(MUMBAI_URL);
+  const provider = new hre.ethers.providers.JsonRpcProvider(FUJI_URL);
 
-  const signer = new hre.ethers.Wallet(PRIVATE_KEY, provider);
+  const signer = new hre.ethers.Wallet(FUJI_PRIVATE_KEY, provider);
 
   const taskList = new hre.ethers.Contract(contractAddress, contractABI, signer);
 
